bccf6b120243e588d8b10960bd66788047a1e666
Omer Esin Yildiz <omeresinyildiz@gmail.com>
2023-01-30 13:02:03 +0100

    Demo_Project

---
+++ RabbiMq.Publisher/RabbiMq.Publisher/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+
+// Baglanti Acma
+
+ConnectionFactory factory = new();
+factory.Uri = new("amqps://tgvytqvu:o4JZc8Hv42aJlr-1aJb32N4JQPE5HAWz@goose.rmq2.cloudamqp.com/tgvytqvu");
+
+// Baglantiyi Aktiflestirme ve Kanal Acma
+using IConnection connection = factory.CreateConnection();
+using IModel channel = connection.CreateModel();
+
+// Queue Olusturma
+channel.QueueDeclare(queue: "example-queue", exclusive: false);
+
+// Queue`ya Mesaj Gonderme
+// RabbitMq kuyruga atacagi mesajlari byte turunde kabul etmektedir. 
+/*byte[] message = Encoding.UTF8.GetBytes("hello, Bro");
+
+channel.BasicPublish("", "example-queue", body:message);*/
+
+for (int i = 0; i < 1000; i++)
+{
+    byte[] message = Encoding.UTF8.GetBytes($"hello, Bro {i}");
+
+    channel.BasicPublish("", "example-queue", body:message);
+}
+
+Console.Read();
---
+++ RabbitMq.Consumer/RabbitMq.Consumer/Program.cs
@@ -0,0 +1,27 @@
+﻿
+// Baglanti Olusturma
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+ConnectionFactory factory = new();
+factory.Uri = new("amqps://tgvytqvu:o4JZc8Hv42aJlr-1aJb32N4JQPE5HAWz@goose.rmq2.cloudamqp.com/tgvytqvu");
+
+// Baglanti Aktiflestirme ve kanal Acma
+using IConnection connection = factory.CreateConnection();
+using IModel channel = connection.CreateModel();
+
+// Queue Olusturma
+channel.QueueDeclare("example-queue", exclusive: false);
+
+// Oueue`dan Mesaj Okuma
+var consumer = new EventingBasicConsumer(channel);
+channel.BasicConsume(queue: "example-queue", true, consumer); // Burada mesaj geldigi zaman consume et dedik
+
+consumer.Received += (sender, args) =>
+{
+    Console.WriteLine(Encoding.UTF8.GetString(args.Body.Span));
+};
+
+Console.Read();
+
